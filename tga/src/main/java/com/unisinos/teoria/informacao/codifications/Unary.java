2d0bfc9710d263c64fc06417d24664e9addd84ab
Joao D. Soares <joao.dacol.soares@gmail.com>
2021-10-03 17:19:18 -0300

    feat: add Unary codificator

---
+++ tga/src/main/java/com/unisinos/teoria/informacao/codifications/Unary.java
@@ -0,0 +1,33 @@
+package com.unisinos.teoria.informacao.codifications;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import htsjdk.samtools.cram.io.BitOutputStream;
+import htsjdk.samtools.cram.io.DefaultBitOutputStream;
+
+public class Unary implements Codification {
+
+  public byte[] compress(int[] asciiLetters) throws IOException {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    BitOutputStream bit = new DefaultBitOutputStream(bytes);
+  
+    for (int i = 0; i < asciiLetters.length; i++) {
+      int asciiLetter = asciiLetters[i];
+
+      for (int j = 0; j < asciiLetter.length(); j++) {
+        bit.write(false);      
+      }
+      bit.write(true);
+    }
+
+    bit.close();
+    
+    return bytes.toByteArray();
+  }
+
+  public int[] decompress(byte[] bytes) {
+    return new int[] {1};
+  }
+   
+}
