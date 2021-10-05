5678ddac434245fa6c11d4d55eb0a4e765f2004a
Guilherme Pohlmann <guilherme.pohlmann@sap.com>
2021-10-05 19:17:24 -0300

    Refactor error correction codes

--- tga/src/main/java/com/unisinos/teoria/informacao/error_correction/CRC.java
+++ tga/src/main/java/com/unisinos/teoria/informacao/error_correction/CRC.java
@@ -5,6 +5,9 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class CRC {
+    private static final String CORRUPTED_CRC_CODE_MESSAGE = "Received crc code is corrupted";
+
+
     public static byte[] encode(byte[] data) throws IOException {
         byte[] polynomial = {1, 0, 0, 0, 0, 0, 1, 1, 1};
         int[] encodedData = new int[data.length + polynomial.length - 1];
@@ -34,5 +37,19 @@ public class CRC {
         }
         dataOutputStream.flush();
         return bytes.toByteArray();
+    }
+
+    public static void validateCRC(byte[] crcCode, byte[] data) throws IllegalArgumentException, IOException {
+        byte[] generatedCRCCode = encode(data);
+
+        if (crcCode.length != generatedCRCCode.length) {
+            throw new IllegalArgumentException(CORRUPTED_CRC_CODE_MESSAGE);
+        }
+
+        for (int i = 0; i < crcCode.length; i++) {
+            if (generatedCRCCode[i] != crcCode[i]) {
+                throw new IllegalArgumentException(CORRUPTED_CRC_CODE_MESSAGE);
+            }
+        }
     }
 }
