4fecee6ed583c31e7b89fbd287718da213337312
Prokopetz <nicolas@prokopetz.com.br>
2021-10-05 22:19:30 -0300

    feat: add delta

---
+++ tga/src/main/java/com/unisinos/teoria/informacao/codifications/Delta.java
@@ -0,0 +1,57 @@
+package com.unisinos.teoria.informacao.codifications;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.unisinos.teoria.informacao.enums.CodificationHeader;
+
+import htsjdk.samtools.cram.io.BitOutputStream;
+import htsjdk.samtools.cram.io.DefaultBitOutputStream;
+
+public class Delta implements Codification {
+
+  public byte[] compress(int[] asciiLetters) throws IOException {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    BitOutputStream bit = new DefaultBitOutputStream(bytes);
+    
+    Codification.writeHeader(bit, CodificationHeader.DELTA);
+
+    List<Integer> encoded = new ArrayList<>();
+    int last = 0;
+    for (int value : asciiLetters) {
+      if (encoded.isEmpty()) {
+        encoded.add(value);
+      } else {
+        encoded.add(value - last);
+      }
+      last = value;
+    }
+    encoded.stream().map(Integer::toBinaryString).forEach((differenceInBinary) -> {
+      for (int i = 0; i < differenceInBinary.length(); i++) {
+        System.out.print(differenceInBinary.charAt(i) == '1' ? "1" : "0");
+        bit.write(differenceInBinary.charAt(i) == '1');
+      }
+    });
+    
+    bit.close();
+    return bytes.toByteArray();
+  }
+
+  public static int[] deltaEncode(int[] buffer) {
+		int original, delta = 0;
+		for (int i = 0; i < buffer.length; ++i) {
+			original = buffer[i];
+			buffer[i] -= delta;
+			delta = original;
+		}
+		return buffer;
+	}
+
+  public int[] decompress(byte[] bytes) throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ tga/src/test/java/com/unisinos/teoria/informacao/codifications/DeltaTest.java
@@ -0,0 +1,18 @@
+package com.unisinos.teoria.informacao.codifications;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class DeltaTest {
+  
+  @Test
+  public void shouldCompress() throws IOException {
+    Codification delta = new Delta();
+    byte[] result = delta.compress(new int[] {12, 13, 14, 15});
+    assertEquals(-128, result[0]);
+  }
+
+}
